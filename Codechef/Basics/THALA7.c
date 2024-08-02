c5f0c43ac4e13283e033731e4bea8fd30625e797
AkaAbhinav <101786960+AkaAbhinav@users.noreply.github.com>
2024-08-02 10:41:53 +0530

    Create THALA7.c

---
+++ Codechef/Basics/THALA7.c
@@ -0,0 +1,20 @@
+/*
+  Author - AkaAbhinav
+  Problem - https://www.codechef.com/problems/THALA7
+  Date - Aug 2 2024
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%i", &N);
+    if (N == 7)
+    {
+        printf("THALA\n");
+    }
+    else
+    {
+        printf("SADGE\n");
+    }
+}
