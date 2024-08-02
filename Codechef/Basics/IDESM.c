b29b085cefacac2eca1ddb93ac0c91a277a1dd46
AkaAbhinav <101786960+AkaAbhinav@users.noreply.github.com>
2024-08-02 10:08:14 +0530

    Create IDESM.c

---
+++ Codechef/Basics/IDESM.c
@@ -0,0 +1,20 @@
+/*
+  Author - AkaAbhinav
+  Problem - https://www.codechef.com/problems/IDESM
+  Date - Aug 2 2024
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%i", &N);
+    if(N==15)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+}
