b76ff42bf427c6bd13fb007d28e722c7e9e919b8
AkaAbhinav <101786960+AkaAbhinav@users.noreply.github.com>
2024-08-02 10:32:17 +0530

    Create CLEARDAY.c

---
+++ Codechef/Basics/CLEARDAY.c
@@ -0,0 +1,16 @@
+/*
+  Author - AkaAbhinav
+  Problem - https://www.codechef.com/problems/CLEARDAY
+  Date - Aug 2 2024
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int X;
+    int Y;
+    scanf("%i  %i", &X, &Y);
+    int a = 7-(X+Y);
+    printf("%i\n", a);
+
+}
