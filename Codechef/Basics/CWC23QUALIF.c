e14846d4d476840748326943f348de2632146b98
AkaAbhinav <101786960+AkaAbhinav@users.noreply.github.com>
2024-08-02 10:01:08 +0530

    Create CWC23QUALIF.c

---
+++ Codechef/Basics/CWC23QUALIF.c
@@ -0,0 +1,20 @@
+/*
+  Author - AkaAbhinav
+  Problem - https://www.codechef.com/problems/CWC23QUALIF
+  Date - Aug 2 2024
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int X;
+    scanf("%i", &X);
+    if(X>=12)
+    {
+    printf("Yes\n");
+    }
+    else
+    {
+    printf("No\n");
+    }
+}
