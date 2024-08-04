24ea1fa46311e1ea7c53f0f80b1fa609d1b7cee6
AkaAbhinav <101786960+AkaAbhinav@users.noreply.github.com>
2024-08-04 13:36:25 +0530

    Create BIRYANI.c

---
+++ Codechef Practice/Basics/BIRYANI.c
@@ -0,0 +1,25 @@
+/*
+  Author - AkaAbhinav
+  Problem - https://www.codechef.com/problems/BIRYANI
+  Date - Aug 4 2024
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int t, x, y;
+    do
+    {
+        scanf("%i", &t);
+    }
+    while (t < 1 || t > 10000);
+    for (int i = 0; i < t; i++)
+    {
+        do
+        {
+            scanf("%i %i", &x, &y);
+            printf("%i\n", x*y);
+        }
+        while ((x < 1 || x > 100) || (y < 1 || y > 100));
+    }
+}
