924e3704ac72ddea96d3da1a6f317687cd96c734
AkaAbhinav <101786960+AkaAbhinav@users.noreply.github.com>
2024-11-22 19:14:49 +0530

    Create NUGGET.c

---
+++ Codechef Practice/Basics/NUGGET.c
@@ -0,0 +1,13 @@
+/*
+  Author - AkaAbhinav
+  Problem - https://www.codechef.com/START161D/problems/NUGGET
+  Date - Nov 20 2024
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    scanf("%i %i", &x, &y);
+    printf("%i", (5000 * x) + (9800 * y));
+}
